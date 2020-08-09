4e0991b044cb95445edf850e0c88f7cc19e069ff
Songfeng <Wusongfeng@MacBook-Pro.local>
2020-08-09 17:22:08 +0800

    project update check

--- dist/npm/@tarojs/taro/dist/index.js
+++ dist/npm/@tarojs/taro/dist/index.js
@@ -362,7 +362,7 @@ var freeGlobal = (typeof global === "undefined" ? "undefined" : _typeof(global))
 var freeSelf = (typeof self === "undefined" ? "undefined" : _typeof(self)) === 'object' && self && self.Object === Object && self;
 /** Used as a reference to the global object. */
 
-var root = freeGlobal || freeSelf || Function('return this')();
+var root = freeGlobal || freeSelf || { Function: Function, Boolean: Boolean, Object: Object, Number: Number, Array: Array, Date: Date, String: String, Symbol: Symbol, Error: Error, TypeError: TypeError, Map: Map, Set: Set, WeakMap: WeakMap, WeakSet: WeakSet, ArrayBuffer: ArrayBuffer, Math: Math, Promise: Promise, RegExp: RegExp, DataView: DataView, isFinite: isFinite, parseInt: parseInt, parseFloat: parseFloat, Float32Array: Float32Array, Float64Array: Float64Array, Int8Array: Int8Array, Int16Array: Int16Array, Int32Array: Int32Array, Uint8Array: Uint8Array, Uint16Array: Uint16Array, Uint32Array: Uint32Array, Uint8ClampedArray: Uint8ClampedArray, setTimeout: setTimeout, clearTimeout: clearTimeout, setInterval: setInterval, clearInterval: clearInterval };
 /** Used as references for various `Number` constants. */
 
 var MAX_SAFE_INTEGER = 9007199254740991;
